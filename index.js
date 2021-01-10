b4703f261f6c288e25486ed1064c7c927371ba98
Sarah Thiery <sarahvthiery@gmail.com>
2021-01-10 15:58:04 -0500

    commit tree 93c5bcfc84fd96828d010dd318dc48895a4a819f

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+
+
+let multiply = num1 * num2 
+
+let random = Math.random() * 10
+
+let num3 = 34
+let num4 = 10
+let mod = num3 % num4
+
+let max = Math.max(2, 4, 10, 20)
